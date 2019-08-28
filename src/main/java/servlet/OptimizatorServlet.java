d49f13554df365d50f095e36bcc95516d9923f53
Mihail Klopotnuk <m.klopotnuk@gmail.com>
2019-08-28 12:45:49 +0300

    Use calc algoritm

--- src/main/java/servlet/OptimizatorServlet.java
+++ src/main/java/servlet/OptimizatorServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @WebServlet("/optim")
@@ -22,17 +23,14 @@ public class OptimizatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Gson gson = new Gson();
-//        String json = req.getReader().readLine();
-//        CalcRequest calcRequest = gson.fromJson(json, CalcRequest.class);
-//        int numUrn = calcRequest.getNumUrn();
-//        List<ModelPoint> points = pointService.getPointsById(calcRequest.getPointIds());
-        List<ModelPoint> points = pointService.getPoints();
-        int numUrn = 2;
+        List<Long> garbageIds = Arrays.asList(gson.fromJson(req.getParameter("garbageIds"),Long[].class));
+        int urnCount = Integer.parseInt(req.getParameter("urnCount"));
+        List<ModelPoint> points = pointService.getPointsById(garbageIds);
         double[][] readyPoints = pointService.preparePointsArray(points);
-        double[][] resultPoints = new Kmeans(readyPoints, numUrn).getCentersArray();
+        double[][] resultPoints = new Kmeans(readyPoints, urnCount).getCentersArray();
         List<UrnPoint> urnPoints = dtoService.createListUrnPoint(resultPoints);
         String respJson = gson.toJson(urnPoints);
-        resp.setContentType("text/html; charset=UTF-8");
-        resp.getWriter().println(respJson);
+        resp.getWriter().write(respJson);
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
