487c5d4ee459b716f6014248f85f3361137c93a6
Mihail Klopotnuk <m.klopotnuk@gmail.com>
2019-08-28 23:06:41 +0300

    remove LocalTime from UrnPoint

--- src/main/java/dto/UrnPoint.java
+++ src/main/java/dto/UrnPoint.java
@@ -6,11 +6,9 @@ public class UrnPoint {
 
     private double lat;
     private double lng;
-    private LocalDateTime time;
 
     public UrnPoint(double lat, double lng) {
         this.lat = lat;
         this.lng = lng;
-        this.time = LocalDateTime.now();
     }
 }
