744c7cdad14295f79dd045f6f3f632600d3204ec
wormogig <wormogig@mail.ru>
2019-08-30 11:22:33 +0300

    Временно отключил отправку письма

--- src/main/java/util/EmailSender.java
+++ src/main/java/util/EmailSender.java
@@ -88,9 +88,9 @@ public class EmailSender {
             );
             message.setSubject("Рекомендация по установке мусорных урн");
             String text = generateMessage(map);
-//            System.out.println(text);
+            System.out.println(text);
             message.setContent(text, "text/html; charset=UTF-8");
-            Transport.send(message);
+//            Transport.send(message);
             System.out.println("Done");
         } catch (MessagingException e) {
             e.printStackTrace();
